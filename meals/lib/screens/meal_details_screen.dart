19fdb405c184a623938eeb87fbbaab4bb01ebb8d
Ian <ian.noel.banta@adamson.edu.ph>
2023-07-18 17:25:26 +0800

    added meal details and bottom navigation bar

---
+++ meals/lib/screens/meal_details_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  const MealDetailsScreen({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(context) {
+    Widget content = SingleChildScrollView(
+      child: Column(
+        children: [
+          FadeInImage(
+            placeholder: MemoryImage(
+              kTransparentImage,
+            ),
+            image: NetworkImage(meal.imageUrl),
+            fit: BoxFit.cover,
+            height: 300,
+            width: double.infinity,
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Text('Ingredients',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.primary)),
+          const SizedBox(
+            height: 14,
+          ),
+          for (final ingredient in meal.ingredients)
+            Text(ingredient,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground)),
+          const SizedBox(
+            height: 14,
+          ),
+          Text('Steps',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.primary)),
+          const SizedBox(
+            height: 14,
+          ),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 8,
+              ),
+              child: Text(step,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground)),
+            ),
+        ],
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(title: Text(meal.title)),
+      body: content,
+    );
+  }
+}
